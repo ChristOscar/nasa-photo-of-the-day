235f231bb74580f9dfd05559c189b59b10551d93
Christwide Oscar <triple101.games@gmail.com>
2021-12-08 17:29:31 -0600

    Able to talk to api and creation of details page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios'
+import { BASE_URL, API_KEY } from "./constants";
+
+
 
 function App() {
+  const [imageId, setImageId] = useState([]);
+  const [imageExp, setImageExp] =  useState(null);
+
+
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+    .then(res => {
+    console.log(res.data);
+    setImageId(res.data);
+    })
+    .catch(err => {
+      console.error(err);
+    })
+  }, [])
+
   return (
     <div className="App">
       <p>
